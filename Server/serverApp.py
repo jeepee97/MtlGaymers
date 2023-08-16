8fb9118527ae0f6e2663cde59375ee22904f9ed2
jeanphilippe anctil <jeanphilippe.anctil@hotmail.com>
2023-08-15 20:53:11 -0400

    remove unused files from prototype

--- Server/serverApp.py
+++ Server/serverApp.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
-from serverAPI import serverAPI_bp
 from routes.userLogin import user_login_bp
-from routes.userLogout import user_logout_bp
 
 class ServerApp:
 	def __init__(self, hostname, port):
@@ -19,9 +17,7 @@ class ServerApp:
 		serverAPI.run(debug=False, host=self.hostname, port=self.port)
 
 	def register_all_blueprints(self, serverAPI):
-		serverAPI.register_blueprint(serverAPI_bp, url_prefix='/server')
 		serverAPI.register_blueprint(user_login_bp, url_prefix='/user')
-		serverAPI.register_blueprint(user_logout_bp, url_prefix="/user")
 
 	def startApp(self):
 		pass
