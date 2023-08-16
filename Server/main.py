0e8ababfc699c21a91f22eab1debf24456d0b42d
jeanphilippe anctil <jeanphilippe.anctil@hotmail.com>
2023-08-15 20:18:02 -0400

    renaming files to fit server

--- Server/main.py
+++ Server/main.py
@@ -1,8 +1,8 @@
-from jukeboxApp import JukeboxApp
+from serverApp import ServerApp
 
 HOST_NAME = "0.0.0.0"
 
 if __name__ == "__main__":
-	app = JukeboxApp(HOST_NAME)
+	app = ServerApp(HOST_NAME)
 	app.start()
 
---
+++ Server/serverApp.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+from serverAPI import serverAPI_bp
+from routes.userLogin import user_login_bp
+from routes.userLogout import user_logout_bp
+
+class ServerApp:
+	def __init__(self, hostname):
+		self.hostname = hostname
+
+	def start(self):
+		self.startAPI()
+		self.startApp()
+
+	def startAPI(self):
+		serverAPI = Flask(__name__)
+		self.register_all_blueprints(serverAPI)
+		serverAPI.run(debug=False, host=self.hostname)
+
+	def register_all_blueprints(self, serverAPI):
+		serverAPI.register_blueprint(serverAPI_bp, url_prefix='/server')
+		serverAPI.register_blueprint(user_login_bp, url_prefix='/user')
+		serverAPI.register_blueprint(user_logout_bp, url_prefix="/user")
+
+	def startApp(self):
+		pass
